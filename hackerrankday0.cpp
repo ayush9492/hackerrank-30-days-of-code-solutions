6944649128e2f31406e20a65ab85d75503bfe1eb
Ayush Bharatiya <74298323+ayush9492@users.noreply.github.com>
2021-02-06 10:07:55 +0530

    Create hackerrankday0.cpp
    source code is in c++ language

---
+++ hackerrankday0.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string inputstring;
+    getline(cin,inputstring);
+    cout<<"Hello, World."<<endl;
+    cout<<inputstring<<endl;
+    
+    return 0;
+}
