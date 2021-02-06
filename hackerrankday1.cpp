31b77912c80df1f73bb3dbe6b3c7fe61e04ff68d
Ayush Bharatiya <74298323+ayush9492@users.noreply.github.com>
2021-02-06 10:05:08 +0530

    Create hackerrankday1.cpp

---
+++ hackerrankday1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    /*enter the second integer, second double, and string*/
+    int i2;
+    double d2;
+    string s2;
+    cin>>i2;
+    cin>>d2;
+    cin.get();
+    getline(cin,s2);
+    /*output lines now */
+    cout<<i+i2<<endl;
+    cout<< std::fixed<<std::setprecision(1)<<d+d2<<endl;
+    cout<<s<<s2;
+
+    return 0;
+}
